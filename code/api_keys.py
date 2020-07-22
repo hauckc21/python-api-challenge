f1220853cb558d4ac832c9d08685ded160df9015
hauckc21 <hauck.chris@gmail.com>
2020-07-22 09:06:33 -0500

    Initial version git commit api keys

--- code/api_keys.py
+++ code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "06b6d3b4e9896c49c7e1dd3759bb5ff7"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA0gZPylfL1dcRCcSj2AcYLDZ7NSETl8G0"
